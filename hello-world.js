0088c383e8231f2d2a0ec8cd7d159d1bfe17ca33
Olusola Ojewunmi <olusolaojewunmi@gmail.com>
2024-09-14 19:59:49 +0100

    Implemented the http and url modules.

--- hello-world.js
+++ hello-world.js
@@ -1,5 +1,6 @@
 const http = require('node:http');
 var dateTime = require('./Modules/DateTimeModule');
+var url = require('url');
 
 const hostname = "127.0.0.1";
 const port = 3000;
@@ -7,6 +8,12 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
+    // res.write(req.url + "\n");
+
+    var q = url.parse(req.url, true).query;
+    var text = q.year + " " + q.month;
+
+    res.write(text + "\n");
     res.write(`The date and time are currently: ${dateTime.myDateTime()}\n`);
     res.end("Hello World!\n");
 });
