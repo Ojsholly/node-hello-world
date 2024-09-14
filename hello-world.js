7b02571cf06c60bb460bb04013606f98b479d42f
Olusola Ojewunmi <olusolaojewunmi@gmail.com>
2024-09-14 19:48:22 +0100

    Implemented module creation and import.

--- hello-world.js
+++ hello-world.js
@@ -1,4 +1,5 @@
 const http = require('node:http');
+var dateTime = require('./Modules/DateTimeModule');
 
 const hostname = "127.0.0.1";
 const port = 3000;
@@ -6,6 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
+    res.write(`The date and time are currently: ${dateTime.myDateTime()}\n`);
     res.end("Hello World!\n");
 });
 
